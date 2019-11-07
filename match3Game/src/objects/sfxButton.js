c49d740ef787a1544700e6c428d40f91c651ba0c
DanyloKopytko <leonelluson@gmail.com>
2019-11-07 19:22:11 +0200

    Darova

--- match3Game/src/objects/sfxButton.js
+++
@@ -1,8 +0,0 @@
-export function createButton(game, x, y, image, width, height, cb) {
-    let button = game.add.button(x, y, image, cb, game);
-
-    button.width = width;
-    button.height = height;
-
-    return button;
-}
