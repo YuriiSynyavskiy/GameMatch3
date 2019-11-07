94f01dcfac378c4412f170feace57d601a86d2ea
YuriiSynyavskiy <53538660+YuriiSynyavskiy@users.noreply.github.com>
2019-11-07 19:52:34 +0200

    mat ebav

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Game extends Phaser.Game {
 		this.state.add('MainMenu', MainMenu, false);
 		this.state.start('MainMenu');
 	}
-//sex with Yura and his mom
+//sex with Danylo and his father
 }
 
 new Game();
